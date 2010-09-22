6721f3029a9d7af63b551710a9726dffa2d729a6
Norman Heino <norman.heino@gmail.com>
2010-09-22 19:28:48 +0200

    Changed: rich text editor to tinyMCE

--- src/widget.xmlliteral.js
+++ src/widget.xmlliteral.js
@@ -5,22 +5,21 @@
  */
 
 RDFauthor.registerWidget({    
-    _loadNicEditor: function () {        
-        if (this._nicEditLoaded && this._domReady) {
-            var editorOptions = {
-                iconsPath: RDFAUTHOR_BASE + 'libraries/nicEditorIcons.gif', 
-                fullPanel: true, 
-                xhtml: true
-            };
-
-            var editor = new nicEditor(editorOptions);
-            editor.setPanel('xmlliteral-edit-panel-' + this.ID);
-            editor.addInstance('xmlliteral-edit-input-' + this.ID);
-            this._editor = editor;
-            
-            // event
-            jQuery('#xmlliteral-edit-input-' + this.ID).change(function () {
-                alert('changed');
+    _loadMCE: function () {
+        if (this._mceLoaded && this._domReady) {
+            $('#xmlliteral-edit-input-' + this.ID).tinymce({
+                script_url: RDFAUTHOR_BASE + 'libraries/tiny_mce/tiny_mce.js', 
+                theme: 'advanced', 
+                skin: 'default', 
+                plugins:  'media, paste, fullscreen, visualchars, xhtmlxtras, safari, wordcount', 
+                // toggletoolbars_status: 'on', 
+                theme_advanced_toolbar_location : 'top', 
+                theme_advanced_toolbar_align : 'left', 
+                theme_advanced_buttons1: 'bold, italic, strikethrough, |, bullist, numlist, blockquote, |, justifyleft, justifycenter, justifyright, |, link, unlink, |, cleanup, fullscreen, code', 
+                theme_advanced_buttons2: 'formatselect, underline, justifyfull, forecolor, |, media, pastetext, pasteword, removeformat, |, charmap, |, outdent, indent, |, undo, redo, help', 
+                theme_advanced_buttons3: '', 
+                theme_advanced_buttons4: '', 
+                theme_advanced_statusbar_location: 'bottom'
             });
         }
     }, 
@@ -28,25 +27,22 @@ RDFauthor.registerWidget({
     // Uncomment this to execute code when your widget is instantiated, 
     // e.g. load scripts/stylesheets etc.
     init: function () {
-        this._nicEditLoaded = false;
-        this._editor        = null;
-        this._domReady      = false;
-        this._datatype      = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral';
+        this._mceLoaded = false;
+        this._domReady  = false;
+        this._datatype  = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral';
         
         var self = this;
-        RDFauthor.loadScript(RDFAUTHOR_BASE + 'libraries/nicEdit.js', function () {
-            self._nicEditLoaded = true;
-            self._loadNicEditor();
+        RDFauthor.loadScript(RDFAUTHOR_BASE + 'libraries/tiny_mce/jquery.tinymce.js', function () {
+            self._mceLoaded = true;
+            self._loadMCE();
         });
-        
-        RDFauthor.loadStylesheet(RDFAUTHOR_BASE + 'src/widget.xmlliteral.css');
     },
     
     // Uncomment this to execute code when you widget's markup is ready in the DOM, 
     // e.g. load jQuery plug-ins, attache event handlers etc.
     ready: function () {
         this._domReady = true;
-        this._loadNicEditor();
+        this._loadMCE();
     },
     
     // return your jQuery-wrapped main input element here
@@ -63,11 +59,10 @@ RDFauthor.registerWidget({
     // return your widget's markup code here
     markup: function () {
         var markup = '\
-            <div class="container xmlliteral-value">\
-                <div id="xmlliteral-edit-panel-' + this.ID + '" class="xmlliteral-edit-panel"></div>\
-                <div id="xmlliteral-edit-input-' + this.ID + '" class="xmlliteral-edit-input">' 
-                    + (this.statement.hasObject() ? this.statement.objectValue() : '') 
-                    + '</div>\
+            <div class="container xmlliteral-value" style="width:' + this.availableWidth() + 'px">\
+                <textarea id="xmlliteral-edit-input-' + this.ID + '" class="xmlliteral-edit-input" style="width:100%">' 
+                    + (this.statement.hasObject() ? this.statement.objectValue() : '') + 
+                '</textarea>\
             </div>';
 
         return markup;
@@ -124,12 +119,7 @@ RDFauthor.registerWidget({
     },
     
     value: function () {
-        var editor = this._editor.instanceById('xmlliteral-edit-input-' + this.ID);
-        if (editor && ('' !== editor.getContent())) {
-            return editor.getContent().replace('&amp;amp;', '&amp;');
-        }
-        
-        return null;
+        return this.element().val();
     }, 
     
     encodeTags: function (stringContainingTags) {
