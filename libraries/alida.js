ebc9d92fb465dccdae3cf271b0798d97ada7ed13
Clemens Hoffmann <cannelony@googlemail.com>
2010-11-01 13:31:02 +0100

    testcommit, addEndpoint, alida.test.html

--- libraries/alida.js
+++ libraries/alida.js
@@ -1,7 +1,10 @@
-Alida = (function ($) {    
+Alida = (function ($) {
+
     var _options = {
         // TODO: defaults
     };
+
+    var _endpoints = new Array();
     
     function _private() {
         
@@ -13,8 +16,12 @@ Alida = (function ($) {
         }, 
         
         addEndpoint: function (endpointURI) {
-            
-        }, 
+            _endpoints.push(endpointURI);
+        },
+
+        getEndpoints: function () {
+            return _endpoints;
+        },
         
         /**
          * var result = {
@@ -30,8 +37,39 @@ Alida = (function ($) {
         query: function (searchString, resultCallback, errorCallback) {
             // TODO: get query result
             // var result = _query();
-            
-            callback(result);
+
+            mainquery = "SELECT DISTINCT ?s ?search\
+                                 WHERE {\
+                                  ?s ?p ?o.\
+                                  FILTER regex(?o, \"" + searchString + "\", \"i\" ).\
+                                  FILTER isLiteral(?o).\
+                                  ?s <http://www.w3.org/2000/01/rdf-schema#label> ?search. } LIMIT 10";
+
+            window.console.info(mainQuery);
+
+            $(endpoints).each( function (i) {
+                //TODO headerabfrage
+                $.ajax( {
+                    type: "GET",
+                    url: 'proxy.php',
+                    data: "contentType=xml&endpoint="+endpoints[i]+"&query="+escape(mainquery),
+                    success: function (data) {
+                        //TODO success
+                        alert('success');
+                    },
+                    error: function (err,txt,errt) {
+                        alert("Error:" + err + " " + txt + " " + errt);
+                    }
+                });
+            });
+
+            if( jQuery.isFunction(resultCallback) ) {
+                resultCallback();
+            }
+
+            if( jQuery.isFunction(errorCallback) ) {
+                errorCallback();
+            }
         }, 
         
         values: function (subjectURI, facetURI) {
