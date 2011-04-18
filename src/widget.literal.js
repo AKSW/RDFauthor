4523c44318e5e49ad65f98c99579310b6c7d39dd
Clemens Hoffmann <cannelony@googlemail.com>
2011-04-18 13:03:20 +0200

    removed readonly attribute

--- src/widget.literal.js
+++ src/widget.literal.js
@@ -202,7 +202,7 @@ RDFauthor.registerWidget({
         var areaMarkup = '\
             <div class="container ' + areaConfig.containerClass + '" style="width:100%">\
                 <div class="notboolean" style="' + ( isBoolean ? 'display:none;' : 'display:block;' ) + '">\
-                <textarea ' + readonly + ' rows="' + String(areaConfig.rows) + '" cols="20" id="literal-value-' +
+                <textarea rows="' + String(areaConfig.rows) + '" cols="20" id="literal-value-' +
                     this.ID + '">' + (this.statement.hasObject() ? this.statement.objectValue() : '') + '</textarea>\
                 </div>\
                 <div class="boolean" style="' + ( isBoolean ? 'display:block;' : 'display:none;' ) + '">\
