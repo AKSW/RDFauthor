4bc0865f121715576074bd7355b232373aa16546
Clemens Hoffmann <cannelony@googlemail.com>
2011-06-10 17:17:38 +0200

    renamed date widget, because of new features (datetimepicker, timepicker), added extra time type to rdfquery in file vcard.html(test)

--- src/rdfauthor.js
+++ src/rdfauthor.js
@@ -1087,7 +1087,7 @@ RDFauthor = (function($, undefined) {
         /* _require(RDFAUTHOR_BASE + 'src/widget.alida.js'); */
         _require(RDFAUTHOR_BASE + 'src/widget.meta.js');
         _require(RDFAUTHOR_BASE + 'src/widget.xmlliteral.js');
-        _require(RDFAUTHOR_BASE + 'src/widget.date.js');
+        _require(RDFAUTHOR_BASE + 'src/widget.datetime.js');
         _require(RDFAUTHOR_BASE + 'src/widget.mailto.js');
         _require(RDFAUTHOR_BASE + 'src/widget.tel.js');
         _requirePending--;
