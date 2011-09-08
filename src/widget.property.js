cc108b5fbd8e03771ff218a9bc3ccd48a3d8f8bf
Clemens Hoffmann <cannelony@googlemail.com>
2011-09-08 14:42:34 +0200

    adds _suggestions function, adds _hasProperties function, shows number of properties, which are everywhere in use

--- src/widget.property.js
+++ src/widget.property.js
@@ -2,6 +2,7 @@
  * This file is part of the RDFauthor project.
  * http://code.google.com/p/rdfauthor
  * Author: Norman Heino <norman.heino@gmail.com>
+ * Author: Clemens Hoffmann <cannelony@gmail.com>
  */
 
 
@@ -53,6 +54,17 @@ RDFauthor.registerWidget({
 
         // load stylesheets
         RDFauthor.loadStylesheet(RDFAUTHOR_BASE + 'src/widget.property.css');
+
+        // returns the size of an object
+        Object.size = function(obj) {
+            var size = 0, key;
+            console.log(obj);
+            for (key in obj) {
+                size++;
+            }
+            return size;
+        }
+
     },
 
     ready: function () {
@@ -69,46 +81,45 @@ RDFauthor.registerWidget({
             <div class="container resource-value">\
                 <input type="text" id="property-input-' + this.ID + '" name="propertpicker" class="text resource-edit-input" />\
             </div>';
-            var propertyPicker = 
-                        '<div id="propertypicker" class="window" style="display: none;">\
-                           <h1 class="title">Suggested Properties\
-                             <br/>\
-                             <input id="filterProperties" autocomplete="off" type="text" class="text inner-label width99" style="margin: 5px 5px 0px 0px;"/>\
-                           </h1>\
-                           <div class="window-buttons">\
-                             <div class="window-buttons-left"></div>\
-                             <div class="window-buttons-right">\
-                               <span class="button button-windowclose"><span>\
-                             </div>\
-                           </div>\
-                           <div class="content">\
-                             <h1 class="propertyHeadline">\
-                               <span style="display: inline-block !important;" class="ui-icon ui-icon-minus"></span>\
-                               <span>In use elsewhere (\
-                               <span id="suggestedInUseCount"></span>\
-                               )</span>\
-                             </h1>\
-                             <div id="suggestedInUse">\
-                             </div>\
-                             <h1 class="propertyHeadline">\
-                               <span style="display: inline-block !important;" class="ui-icon ui-icon-minus"></span>\
-                               <span>Gerenal applicable (\
-                               <span id="suggestedGernalCount"></span>\
-                               )</span>\
-                             </h1>\
-                             <div id="suggestedGernal">\
-                             </div>\
-                             <h1 class="propertyHeadline">\
-                               <span style="display: inline-block !important;" class="ui-icon ui-icon-plus"></span>\
-                               <span>Applicable (\
-                                <span id="suggestedApplicableCount"></span>\
-                               )</span>\
-                             </h1>\
-                             <div id="suggestedApplicable">\
-                             </div>\
-                          </div>\
-                         </div>\
-                        ';
+            var propertyPicker = '\
+                <div id="propertypicker" class="window" style="display: none;">\
+                  <h1 class="title">Suggested Properties\
+                    <br/>\
+                    <input id="filterProperties" autocomplete="off" type="text" class="text inner-label width99" style="margin: 5px 5px 0px 0px;"/>\
+                  </h1>\
+                  <div class="window-buttons">\
+                    <div class="window-buttons-left"></div>\
+                    <div class="window-buttons-right">\
+                      <span class="button button-windowclose"><span>\
+                    </div>\
+                  </div>\
+                  <div class="content">\
+                    <h1 class="propertyHeadline">\
+                      <span style="display: inline-block !important;" class="ui-icon ui-icon-minus"></span>\
+                      <span>In use elsewhere (\
+                      <span id="suggestedInUseCount"></span>\
+                      )</span>\
+                    </h1>\
+                    <div id="suggestedInUse">\
+                    </div>\
+                    <h1 class="propertyHeadline">\
+                      <span style="display: inline-block !important;" class="ui-icon ui-icon-minus"></span>\
+                      <span>Gerenal applicable (\
+                      <span id="suggestedGernalCount"></span>\
+                      )</span>\
+                    </h1>\
+                    <div id="suggestedGernal">\
+                    </div>\
+                    <h1 class="propertyHeadline">\
+                      <span style="display: inline-block !important;" class="ui-icon ui-icon-plus"></span>\
+                      <span>Applicable (\
+                       <span id="suggestedApplicableCount"></span>\
+                      )</span>\
+                    </h1>\
+                    <div id="suggestedApplicable">\
+                    </div>\
+                 </div>\
+                </div>';
                     
         if( $('#propertypicker').length == 0 ) {
             $('body').append(propertyPicker);
@@ -212,6 +223,75 @@ RDFauthor.registerWidget({
         return prefixedName;
     },
 
+    _suggestions: function (callback) {
+        var self = this;
+        var subjectURI = self.statement.subjectURI();
+        var graphURI = self.statement.graphURI();
+        var prefixPattern = '\
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n';
+        var selectPattern = 'DISTINCT ?resourceUri ?label\n';
+        var typePattern = '<' + subjectURI + '> a ?class .\n';
+        var classPattern = '?others a ?class .\n';
+        var uriPattern = '?others ?resourceUri ?object .\n';
+        var labelPattern = 'OPTIONAL {?resourceUri rdfs:label ?label . } .\n';
+        var query = prefixPattern + 'SELECT ' + selectPattern 
+                                  + 'WHERE { \n' 
+                                  + typePattern
+                                  + classPattern
+                                  + uriPattern
+                                  + labelPattern
+                                  + '}';
+        var propertiesInUse = {};
+        // request properties in use
+        RDFauthor.queryGraph(graphURI, query, {
+            callbackSuccess: function(data) {
+                var results = data.results.bindings;
+                for (var i in results) {
+                    var resourceUri = results[i].resourceUri.value;
+                    typeof(results[i].label) != "undefined" ? propertiesInUse[resourceUri] = results[i].label.value
+                                                            : propertiesInUse[resourceUri] = null;
+                }
+                self._hasProperties(function(hasProperties){
+                    for (var resourceUri in propertiesInUse) {
+                        $.inArray(resourceUri, hasProperties) != -1 ? delete propertiesInUse[resourceUri]
+                                                                    : null;
+                    }
+                    $.isFunction(callback) ? callback(propertiesInUse) : null;
+                })
+            }
+        });
+    },
+
+    _hasProperties: function (callback) {
+        var self = this;
+        var subjectURI = self.statement.subjectURI();
+        var graphURI = self.statement.graphURI();
+        var prefixPattern = '\
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n';
+        var selectPattern = '?resourceUri\n';
+        var uriPattern = '<' + subjectURI + '> ?resourceUri ?object .\n';
+        var query = prefixPattern + 'SELECT ' + selectPattern 
+                                  + 'WHERE { \n' 
+                                  + uriPattern
+                                  + '}';
+        var hasProperties = [];
+        //query
+        RDFauthor.queryGraph(graphURI, query, {
+            callbackSuccess: function(data) {
+                var results = data.results.bindings;
+                for (var i in results) {
+                    hasProperties.push(results[i].resourceUri.value);
+                }
+                $.isFunction(callback) ? callback(hasProperties) : null;
+            },
+            callbackError: function() {
+                $.isFunction(callback) ? callback(hasProperties) : null;
+            }
+        });
+    },
+
     _normalizeValue: function (value) {
         if (!this.selectedResource) {
             this.selectedResource      = this.expandNamespace(value);
@@ -233,6 +313,10 @@ RDFauthor.registerWidget({
                                     .css('top',top)
                                     .data('input',$(this))
                                     .show();
+                // query
+                self._suggestions(function(propertiesInUse) {
+                    $('#suggestedInUseCount').html(Object.size(propertiesInUse));
+                });
             }).keydown(function (e) {
                 if ((e.which === 13) && self._options.selectOnReturn) {
                     $('#propertypicker').hide();
@@ -256,6 +340,7 @@ RDFauthor.registerWidget({
                 }
             });
 
+            /** SHOW-HIDE-SCROLL EVENTS */
             $('html').click(function(){
                 if ($('#propertypicker').css("display") != "none" && focus == false) {
                     $('#propertypicker').fadeOut();
