7f23ee4454a0bc26759af39eb40882e2b3246950
Clemens Hoffmann <cannelony@googlemail.com>
2011-09-21 16:49:34 +0200

    adds spinner

--- src/widget.property.js
+++ src/widget.property.js
@@ -127,9 +127,24 @@ RDFauthor.registerWidget({
                  </div>\
                 </div>';
         var modalwrapper = '<div class="modal-wrapper-propertyselector"></div>';
+        var spinner = '<div id="spinner-propertyselector">\
+            <div class="bar1"></div>\
+            <div class="bar2"></div>\
+            <div class="bar3"></div>\
+            <div class="bar4"></div>\
+            <div class="bar5"></div>\
+            <div class="bar6"></div>\
+            <div class="bar7"></div>\
+            <div class="bar8"></div>\
+            <div class="bar9"></div>\
+            <div class="bar10"></div>\
+            <div class="bar11"></div>\
+            <div class="bar12"></div>\
+          </div>'
         if( $('#propertypicker').length == 0 ) {
             $('body').append(modalwrapper);
-            $('.modal-wrapper-propertyselector').append(propertyPicker);
+            $('.modal-wrapper-propertyselector').append(spinner)
+                                                .append(propertyPicker);
         }
 
         return markup;
@@ -358,6 +373,7 @@ RDFauthor.registerWidget({
                     $('#suggestedGeneralCount').html(Object.size(generalapplicable));
                     // ready
                     self._positioning();
+                    $('#spinner-propertyselector').remove();
                     $('#propertypicker').fadeIn();
                 });
             }).keydown(function (e) {
@@ -454,7 +470,7 @@ RDFauthor.registerWidget({
         return pos;
     },
 
-    _positioning: function () {
+    _positioning: function (element) {
         //trick to get the height and width from a non visible object using jquery
         var bodyh = $(document).height();
         var bodyw = $(document).width();
