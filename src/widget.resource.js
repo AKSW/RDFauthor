654154b74e033cdd916c447862b1a2b8bf027aa2
Clemens Hoffmann <cannelony@googlemail.com>
2012-04-13 15:06:46 +0200

    fix not changeable uri (resource widgeti)

--- src/widget.resource.js
+++ src/widget.resource.js
@@ -484,6 +484,13 @@ RDFauthor.registerWidget({
                        .removeClass('resource-autocomplete-uri-name');
                 }
             }).blur(function() {
+                if (!($(this).data('uri') == $(this).val())) {
+                    console.log('hier müsste dann uri und label und hasLabel neugesetzt werden');
+                    var value = $(this).val();
+                    $(this).data('uri', value);
+                    $(this).data('label', value);
+                    $(this).data('hasLabel', false);
+                }
                 if ($(this).data('hasLabel')) {
                     $(this).val($(this).data('label'))
                        .removeClass('resource-autocomplete-uri')
