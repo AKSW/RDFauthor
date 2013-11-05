fdf11876a20785b34f8aaeba653f9a88d48b4919
Andreas Nareike <nareike@informatik.uni-leipzig.de>
2013-11-05 14:41:30 +0100

    first test to submit values without reloading
    This is a first test to see if it is possible to submit value
    with RDFauthor without reloading the whole site each time.
    There are many known issues that have to be treated later. Also it will
    right now only work for literal and resource widgets. Also the reloading
    has to be disabled in the support.js (silverblue theme), which is done in a
    separate commit.

--- src/rdfauthor.inlinecontroller.js
+++ src/rdfauthor.inlinecontroller.js
@@ -54,6 +54,100 @@ InlineController.prototype = {
         return submitOk;
     },
 
+    resetToUnedit: function(values) {
+        var updateValues = values;
+        var predicateURI;
+        var predicateCount = 0;
+
+        for (var index in this._rows) {
+            // test if there is a value for a new predicate
+            // roughly speaking, the predicateCount will 
+            if (predicateURI == this._rows[index]._predicateURI) {
+                predicateCount += 1;
+            }
+            else {
+                predicateURI = this._rows[index]._predicateURI;
+                predicateCount = 1;
+            }
+            var element = jQuery('#' + this._rows[index].cssID()).parent();
+            console.log('element: ', element);
+
+            // make visible again and disable edit mode
+            // only necessary for the first of (possibly) many equal
+            // predicates
+            if (predicateCount == 1) {
+                element.removeAttr('class');
+                element.attr('class', 'has-contextmenu-area');
+                element.children('.contextmenu').removeAttr('style');
+                element.children('.bullets-none').removeAttr('style');
+            }
+
+            var widgetCount = 0;
+            for (var wid in this._rows[index]._widgets) {
+                var newVal = updateValues.shift();
+                widgetCount += 1;
+                console.log("---> ", predicateCount, widgetCount);
+                if (widgetCount > 1) {
+                    var li = element.find('ul li:eq(' + (predicateCount + widgetCount - 3) + ')');
+                    var newLi = $(li.clone());
+                    newLi.data('rdfauthor.statement', li.data('rdfauthor.statement'));
+                    li.after(newLi);
+                    console.log("inserting an element: ", newLi);
+                }
+                var li = element.find('ul li:eq(' + (predicateCount + widgetCount - 2) + ')');
+                console.log('Li-Element: ', li);
+                console.log('Inherent Data: ', li.data());
+                console.log('Update with: ', newVal);
+                var widgetType;
+                try {
+                    widgetType = this._rows[index]._widgets[wid].getWidgetType();
+                }
+                catch (exception) {
+                    widgetType = null;
+                }
+                switch(widgetType) {
+                    case 'literal':
+                        li.text(newVal);
+                        li.attr('content', newVal);
+                        var oldStatement = li.data('rdfauthor.statement');
+                        var newStatement = oldStatement.copyWithObject(newVal);
+                        li.removeData();
+                        // TODO: update hash?!
+                        li.removeAttr('data-object-hash');
+                        li.data('rdfauthor.statement', newStatement);
+                        break;
+                    case 'resource':
+                        console.log('Inherent Data of first <a>: ', li.children('a:eq(0).data()'));
+                        console.log('Inherent Data of second <a>: ', li.children('a:eq(1).data()'));
+                        /* TODO: for 'rdfs:seeAlso' and possibly other predicates,
+                         * there is another li-element here
+                         */
+                        li.children('a:eq(0)').text(newVal);
+                        // update *all* a children (there are two for rdfs:seeAlso)
+                        li.children('a').attr('resource', newVal);
+                        var href = li.children('a:eq(0)').attr('href');
+                        // TODO: use 'correct' way instead of kludge
+                        href = RDFAUTHOR_BASE.split('/').slice(0, -3).join("/") + '/view/?r=' + newVal;
+                        li.children('a:eq(0)').attr('href', href);
+                        var oldStatement = li.children('a:eq(0)').data('rdfauthor.statement');
+                        var newStatement = oldStatement.copyWithObject(newVal);
+                        console.log(newStatement);
+                        li.children('a:eq(0)').removeData();
+                        li.children('a:eq(0)').data('rdfauthor.statement', newStatement);
+                        // remove autocomplete box (_always_ on page but hidden)
+                        $('.ui-autocomplete').remove();
+                        break;
+                    default:
+                        window.setTimeout(function () {
+                            window.location.href = window.location.href;
+                        }, 1000);
+                }
+            }
+        }
+        // Remove all widgets that RDFauthor has opened
+        $('div.rdfauthor-predicate-row').remove();
+    },
+
     cancel: function () {
         for (var index in this._rows) {
             result &= this._rows[index].cancel();
