7d7a74fcc6b29b89cb04112aec7c3bbbc6fe3549
Clemens Hoffmann <cannelony@gmail.com>
2013-11-14 21:35:45 +0100

    add on change events of datatype or language

--- src/widgets/rdfauthor.widget.literal.js
+++ src/widgets/rdfauthor.widget.literal.js
@@ -37,28 +37,28 @@ RDFauthor.getInstance(function(RDFauthorInstance) {
     
     markup: function () {
       // indicates a set language or datatype
-      var langClass = this.hasLanguageTag() ? 'language' : '';
-      var datatypeClass = this.hasDatatype() ? 'datatype' : '';
+      var langClass = this.hasLanguageTag() ? 'hasLanguage' : '';
+      var datatypeClass = this.hasDatatype() ? 'hasDatatype' : '';
       
       // generate datatype submenu markup
       var datatypeSubmenuMarkup = '\
         <li class="dropdown-submenu ' + datatypeClass + '">\
-          <a tabindex="-1" href="#">Datatype</a>\
+          <a tabindex="-1" href="#" class="set-datatype">Datatype</a>\
           <ul class="dropdown-menu">';
       // iterate through predefined datatypes
       var matchedType = false;
       for (var type in this.datatypes) {
         if (type.indexOf(this.statement.objectDatatype()) > -1) {
           matchedType = true;
-          datatypeSubmenuMarkup +='<li><a tabindex="-1" href="#" name="' + type + '" class="' + datatypeClass + '">' + this.datatypes[type] + '</a></li>';
+          datatypeSubmenuMarkup +='<li><a tabindex="-1" href="#" name="' + type + '" class="datatype ' + datatypeClass + '">' + this.datatypes[type] + '</a></li>';
         } else {
-          datatypeSubmenuMarkup +='<li><a tabindex="-1" href="#" name="' + type + '">' + this.datatypes[type] + '</a></li>';          
+          datatypeSubmenuMarkup +='<li><a tabindex="-1" href="#" name="' + type + '" class="datatype">' + this.datatypes[type] + '</a></li>';          
         }
         console.log('type ', type);
       }
       // if type is not part of rdfauthors datatype set, put it at the end
       if (!matchedType && this.statement.objectDatatype() != null) {
-        datatypeSubmenuMarkup += '<li><a tabindex="-1" href="#" name="' + this.statement.objectDatatype() + '" class="' + datatypeClass + '">' + this.statement.objectDatatype() + '</a></li>';
+        datatypeSubmenuMarkup += '<li><a tabindex="-1" href="#" name="' + this.statement.objectDatatype() + '" class="datatype ' + datatypeClass + '">' + this.statement.objectDatatype() + '</a></li>';
       }
       // close ul and li
       datatypeSubmenuMarkup += '</ul></li>';
@@ -66,21 +66,21 @@ RDFauthor.getInstance(function(RDFauthorInstance) {
       // generate language submenu markup
       var languageSubmenuMarkup = '\
         <li class="dropdown-submenu ' + langClass + '">\
-          <a tabindex="-1" href="#">Language</a>\
+          <a tabindex="-1" href="#" class="set-language">Language</a>\
           <ul class="dropdown-menu">';    
       // iterate through predefined datatypes
       var matchLang = false;
       for (var i in this.languageTags) {
         if (this.languageTags[i].indexOf(this.statement.objectLanguage()) > -1) {
           matchLang = true;
-          languageSubmenuMarkup +='<li><a tabindex="-1" href="#" name="' + this.languageTags[i] + '" class="' + langClass + '">' + this.languageTags[i] + '</a></li>';
+          languageSubmenuMarkup +='<li><a tabindex="-1" href="#" name="' + this.languageTags[i] + '" class="language ' + langClass + '">' + this.languageTags[i] + '</a></li>';
         } else {
-          languageSubmenuMarkup +='<li><a tabindex="-1" href="#" name="' + this.languageTags[i] + '">' + this.languageTags[i] + '</a></li>';        
+          languageSubmenuMarkup +='<li><a tabindex="-1" href="#" name="' + this.languageTags[i] + '" class="language">' + this.languageTags[i] + '</a></li>';        
         }
       }
       // if language is not part of rdfauthors rdfauthor set, put it at the end
       if (!matchLang && this.statement.objectLanguage() != null) {
-        languageSubmenuMarkup +='<li><a tabindex="-1" href="#" name="' + this.statement.objectLanguage() + '" class="' + langClass + '">' + this.statement.objectLanguage() + '</a></li>';
+        languageSubmenuMarkup +='<li><a tabindex="-1" href="#" name="' + this.statement.objectLanguage() + '" class="language ' + langClass + '">' + this.statement.objectLanguage() + '</a></li>';
       } 
       // close ul and li
       languageSubmenuMarkup += '</ul></li>';
@@ -108,8 +108,44 @@ RDFauthor.getInstance(function(RDFauthorInstance) {
       console.log('rdy called for literal widget');
       // jquery events
       console.log('elementDropdown', this.elementDropdown());
+      
+      // remove object
       $(self.elementDropdown()).on('click', '.remove', function (event) {
         self.remove();
+      });
+      
+      // set language tag
+      $(self.elementDropdown()).on('click', '.language', function (event) {
+        console.log('click on language tag', $(this).attr('name'));
+        var $prevTag = $(this).parents('.dropdown-submenu').find('a.hasLanguage');
+        var $curTag = $(this);
+        // remove all hasLanguage or hasDatatype classes
+        $curTag.parents('.input-group-btn').find('.hasLanguage').removeClass('hasLanguage');
+        $curTag.parents('.input-group-btn').find('.hasDatatype').removeClass('hasDatatype');
+        // toggle hasLanguage if choosen lang tag was prev lang tag
+        if ($prevTag.attr('name') != $curTag.attr('name')) {
+          // set choosen language tag
+          $curTag.addClass('hasLanguage');
+          $curTag.parents('.dropdown-submenu').addClass('hasLanguage');
+          $curTag.parents('.input-group-btn').find('button').addClass('hasLanguage');
+        }
+      });
+      
+      // set datatype tag
+      $(self.elementDropdown()).on('click', '.datatype', function (event) {
+        console.log('click on datatype tag', $(this).attr('name'));
+        var $prevType = $(this).parents('.dropdown-submenu').find('a.hasDatatype');
+        var $curType = $(this);
+        // remove all hasLanguage or hasDatatype classes
+        $curType.parents('.input-group-btn').find('.hasLanguage').removeClass('hasLanguage');
+        $curType.parents('.input-group-btn').find('.hasDatatype').removeClass('hasDatatype');
+        // toggle hasLanguage if choosen lang tag was prev lang tag
+        if ($prevType.attr('name') != $curType.attr('name')) {
+          // set choosen language tag
+          $curType.addClass('hasDatatype');
+          $curType.parents('.dropdown-submenu').addClass('hasDatatype');
+          $curType.parents('.input-group-btn').find('button').addClass('hasDatatype');
+        }
       });
     },
     
