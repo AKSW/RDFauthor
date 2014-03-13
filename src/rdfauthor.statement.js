AKSW/RDFauthor

commit d5f6dabbd276f8f1f58cc7bc2ceadc1b445b3c38

copyWithObject will respect a passed type
When editing with RDFauthor, there is a method called
copyWithObject that copies a statement's subject and predicate, but
exchanges the old object for a new one. However, there is a line of code
that explictly adds the type of the old object to the new object.
We now check first if the new object has a type and only add the old type
if no type is given.

src/rdfauthor.statement.js
@@ -187,7 +187,9 @@ Statement.prototype = {
      *
      */
     copyWithObject: function (objectSpec) {
-        jQuery.extend(objectSpec, {type: this.objectType()});
+        if (!("type" in objectSpec)) {
+            jQuery.extend(objectSpec, {type: this.objectType()});
+        }
 
         var copy = new Statement({
             subject: '<' + this.subjectURI() + '>',

commit 20a5f4fe55dda7a5e35196b40c2e86ee99c84a18

Fix a bug in rdfauthor.statement.js
This commit fixes a bug that was introduced with commit
d5f6dabbd276f8f1f58cc7bc2ceadc1b445b3c38

src/rdfauthor.statement.js
@@ -187,7 +187,7 @@ Statement.prototype = {
      *
      */
     copyWithObject: function (objectSpec) {
-        if (!("type" in objectSpec)) {
+        if (!(typeof objectSpec === 'object') || !("type" in objectSpec)) {
             jQuery.extend(objectSpec, {type: this.objectType()});
         }
 